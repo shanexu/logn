aea353bb979d72ed73d844e2b25ce34e007e54fd
Shane Xu <xusheng0711@gmail.com>
2019-01-20 16:31:20 +0800

    add test

--- config/config.go
+++ config/config.go
@@ -7,6 +7,12 @@ type Config struct {
 	Loggers   Loggers                     `config:"loggers"`
 }
 
+var defaultConfig Config
+
+func DefaultConfig() Config {
+	return defaultConfig
+}
+
 type Loggers struct {
 	Root   RootLogger `config:"root"`
 	Logger []Logger   `config:"logger"`
@@ -20,4 +26,24 @@ type RootLogger struct {
 type Logger struct {
 	Name         string   `config:"name"`
 	AppenderRefs []string `config:"appender_refs"`
+}
+
+func init() {
+	defaultConfig = Config{
+		Appenders: map[string][]*common.Config{
+			"console": {common.MustNewConfigFrom(`
+name: CONSOLE
+target: stdout
+encoder:
+  json:
+`)},
+		},
+		Loggers: Loggers{
+			Root: RootLogger{
+				AppenderRefs: []string{"CONSOLE"},
+				Level:        "info",
+			},
+			Logger: nil,
+		},
+	}
 }
--- core/core.go
+++ core/core.go
@@ -1,14 +1,22 @@
 package core
 
 import (
-	"github.com/pkg/errors"
-	"github.com/shanexu/logp/config"
+	"errors"
+	"github.com/shanexu/logp/appender"
+	"github.com/shanexu/logp/common"
+	"go.uber.org/zap/zapcore"
+	"sync"
 )
 
 type Core struct {
-
+	nameToLogger   sync.Map
+	nameToAppender map[string]appender.Appender
+	rootAppender   *appender.Appender
+	rootLevel      zapcore.Level
 }
 
-func New(rawConfig config.Config) (*Core, error) {
+func New(rawConfig *common.Config) (*Core, error) {
+	rawConfig.Unpack()
+
 	return nil, errors.New("not implemented")
 }
