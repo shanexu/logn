4d5bd53dbe827960e9b68b66b71008c7fa74263d
Shane Xu <xusheng0711@gmail.com>
2019-01-21 17:54:24 +0800

    rename file

--- appenders.go
+++
@@ -1,8 +0,0 @@
-package logn
-
-import (
-	_ "github.com/shanexu/logn/appender"
-	_ "github.com/shanexu/logn/appender/console"
-	_ "github.com/shanexu/logn/appender/file"
-	_ "github.com/shanexu/logn/appender/rollingfile"
-)
--- core/includes.go
+++ core/includes.go
@@ -2,9 +2,10 @@ package core
 
 import (
 	_ "github.com/shanexu/logn/appender"
-	_ "github.com/shanexu/logn/appender/console"
-	_ "github.com/shanexu/logn/appender/file"
-	_ "github.com/shanexu/logn/appender/rollingfile"
+
+	_ "github.com/shanexu/logn/appender/writer/console"
+	_ "github.com/shanexu/logn/appender/writer/file"
+	_ "github.com/shanexu/logn/appender/writer/rollingfile"
 
 	_ "github.com/shanexu/logn/appender/encoder"
 	_ "github.com/shanexu/logn/appender/encoder/json"
